9f02fc6bb5ff7817f27b3ace66f58c65ecf73829
DesktopMad <cvf223@me.com>
2018-05-18 18:22:03 +0200

    sdc
    change

--- testshell.sh
+++ testshell.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 GEN_PASS=`pwgen -1 20 -n`
-
-IP_ADD=`curl ipinfo.io/ip`
-
+IP_ADD=`shuf -i 10000-50000 -n1`
 IP_1=199.233.388.444
 IP2=22.222.2222.22
-
 string=HalloWorld
-
-mkdir .testfolder
-
+mkdir .testfolder2
 masternodekey=$(./qbic-cli masternode genkey)
-
-echo -e "masternode=1\nmasternodeprivkey=$masternodekey\nGen_pass=${GEN_PASS}\nIP_ADD=${IP_ADD}\nIP_1=$IP_1\nIP2=${IP2}\nstring=$string\nstring=${string}" >> /root/.qbiccore/qbic.conf
-
-
+echo -e "masternode=1\nmasternodeprivkey=$masternodekey\nGen_pass=${GEN_PASS}\nIP_ADD=${IP_ADD}\nIP_1=$IP_1\nIP2=${IP2}\nstring=$string\nstring=${string}" >> /root/.testfolder2/test.conf
 echo "Masternode private key: $masternodekey"
-
 echo "Welcome to the QBIC world"
-
 echo "IP_1: $IP_1"
-
 echo "IP_1: ${IP_1}"
-
 echo "string: $string"
 echo "string: ${string}"
-
 echo "Welcome to the QBIC world"
