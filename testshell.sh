dc5df646672077e85c6b4a2adf3c1cd777e44a78
DesktopMad <cvf223@me.com>
2018-05-19 13:19:32 +0200

    aa

--- testshell.sh
+++ testshell.sh
@@ -3,7 +3,7 @@ GEN_PASS=`pwgen -1 20 -n`
 IP_ADD=`shuf -i 10000-50000 -n1`
 IP_1=199.233.388.444
 IP2=22.222.2222.22
-string={HalloWorld}
+string=$HalloWorld
 mkdir .testfolder2
 masternodekey=$(./qbic-cli masternode genkey)
 echo -e "masternode=1\nmasternodeprivkey=$masternodekey\nGen_pass=${GEN_PASS}\nIP_ADD=${IP_ADD}\nIP_1=$IP_1\nIP2=${IP2}\nstring=$string\nstring=${string}" >> /root/.testfolder2/test.conf
