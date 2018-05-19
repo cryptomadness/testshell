621f5fa05efccf69ed7c92c89d2ab335a2250f37
DesktopMad <cvf223@me.com>
2018-05-19 13:14:31 +0200

    deleat

--- testshell.sh
+++ testshell.sh
@@ -4,7 +4,6 @@ IP_ADD=`shuf -i 10000-50000 -n1`
 IP_1=199.233.388.444
 IP2=22.222.2222.22
 string=HalloWorld
-mkdir .testfolder2
 masternodekey=$(./qbic-cli masternode genkey)
 echo -e "masternode=1\nmasternodeprivkey=$masternodekey\nGen_pass=${GEN_PASS}\nIP_ADD=${IP_ADD}\nIP_1=$IP_1\nIP2=${IP2}\nstring=$string\nstring=${string}" >> /root/.testfolder2/test.conf
 echo "Masternode private key: $masternodekey"
